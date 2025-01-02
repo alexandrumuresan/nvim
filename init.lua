1a0bfb511501c0c918d94917a5792272b2f287a8
AlexM <alexander.muresan@gmail.com>
2025-01-02 18:35:23 +0200

    changing tab width from 4 to 2 spaces

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
