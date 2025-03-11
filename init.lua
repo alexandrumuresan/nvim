55d6292c763d9f6a2ddcc3c8c2f86163ade0c0ac
AlexM <alexander.muresan@gmail.com>
2025-03-11 19:04:43 +0200

    Adding tailwind plugins

--- init.lua
+++ init.lua
@@ -525,6 +525,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
+        tailwindcss = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
